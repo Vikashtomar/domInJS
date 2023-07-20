8c15766a34ccdfd966127791837dcdae3dc2bfa2
vikash <vikash.tomar.india@gmail.com>
2023-07-20 19:02:27 +0530

    dom files

---
+++ addevent.js
@@ -0,0 +1,6 @@
+let ele = document.getElementById("box")
+ele.addEventListener('click',myfun);
+function myfun()
+{
+    ele.style.background="green";
+}
---
+++ clone_node.js
@@ -0,0 +1,27 @@
+ // Clone Node Method
+      // -> Clone Node Method is used to copy element or textNode in html by js
+
+      // Syntax:
+
+      // cloneNode(true);
+      // -> true value will return the tag and content whereas
+      // -> false value will give only tag not the content.
+
+      // let list = document.getElementById("first").firstElementChild;
+
+      // let copyele = list.cloneNode(true)
+      // console.log(copyele)
+      // let second = document.getElementById("second").appendChild(copyele);
+
+      // ---------------------------------------------------------------------------------------
+
+      // Contains Method
+      // Contains method will search id/class in the html if that element will found than it will return true  otherwise it will give  the  false.
+
+      // -> the element can be child or grand child.
+
+    //   let tar = document.getElementById("one");
+    //   let b = document.getElementById("para");
+
+    //   let op = tar.contains(b);
+    //   console.log(op);
